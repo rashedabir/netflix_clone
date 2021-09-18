e06b6b6475806797c065f17853de0cc47e016744
rashedabir <rashed15-2155@diu.edu.bd>
2021-09-18 19:54:20 +0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="font-bold contaoner">hellow</h1>
+    </div>
+  );
+}
+
+export default App;
