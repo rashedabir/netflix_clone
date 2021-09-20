7c438cad29685e09a8295c64b2379003f2f5482c
rashedabir <rashed15-2155@diu.edu.bd>
2021-09-20 21:16:46 +0600

    fourth commit

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,29 +1,57 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ListItem from "./ListItem";
 
 function List() {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    const distance = listRef.current.getBoundingClientRect().x - 38;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${235 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-235 + distance}px)`;
+    }
+  };
+
   return (
     <div className="w-full list mt-2.5">
       <span className="ml-12 text-xl font-bold text-white capitalize listtitle">
         continue to watch
       </span>
       <div className="relative flex items-center wrapper">
-        <i className="py-10 text-5xl text-white bg-gray-900 absolt-0 fas fa-chevron-left"></i>
-        <div className="flex ml-2 w-max">
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+        <i
+          style={{ display: !isMoved && "none" }}
+          className="absolute top-0 bottom-0 left-0 z-50 py-10 text-5xl text-white bg-gray-900 fas fa-chevron-left"
+          onClick={() => {
+            handleClick("left");
+          }}
+        ></i>
+        <div
+          className="flex ml-10 transition-all duration-1000 ease-linear transform translate-x-0 w-max"
+          ref={listRef}
+        >
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4} />
+          <ListItem index={5} />
+          <ListItem index={6} />
+          <ListItem index={7} />
+          <ListItem index={8} />
         </div>
-        <i className="absolute right-0 py-10 text-5xl text-white bg-gray-900 py65 worder-2 fas fa-chevron-right"></i>
+        <i
+          className="absolute top-0 bottom-0 right-0 z-50 py-10 text-5xl text-white bg-gray-900 py65 worder-2 fas fa-chevron-right"
+          onClick={() => {
+            handleClick("right");
+          }}
+        ></i>
       </div>
     </div>
   );
--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
-function ListItem() {
-  return <div className="h-32 mr-2 text-black bg-white w-60">list item</div>;
+function ListItem({ index }) {
+  const [isHover, setIsHover] = useState(false);
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
+  return (
+    <div
+      className="h-32 mr-2 overflow-hidden text-white cursor-pointer w-60 list_item"
+      style={{
+        backgroundColor: "#0b0b0b",
+        left: isHover && index * 255 - 50 + index * 2.5,
+      }}
+      onMouseEnter={() => {
+        setIsHover(true);
+      }}
+      onMouseLeave={() => {
+        setIsHover(false);
+      }}
+    >
+      <img
+        className="object-cover w-full h-full"
+        src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/08/marvels-avengers.jpg"
+        alt=""
+      />
+      {isHover && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="flex flex-col p-5 item_info">
+            <div className="icons">
+              <i className="fas fa-play"></i>
+              <i className="fas fa-plus"></i>
+              <i className="fas fa-thumbs-up"></i>
+              <i className="fas fa-thumbs-down"></i>
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 46 mins</span>
+              <span>+16</span>
+              <span>2001</span>
+            </div>
+            <div>
+              In publishing and graphic design, Lorem ipsum is a placeholder
+              text commonly used to demonstrate the visual form.
+            </div>
+            <div>action</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ListItem;
